c62d96719c5652a2da3c7ff528a174b493977672
holdenn1 <caulfieldd17@gmail.com>
2022-12-13 21:53:28 +0200

    add users on db and added dinamic users loading

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
 'use strict';
+
 const users = document.querySelector('.users');
-const url = 'http://localhost:3000/users?_start=0&_end=8';
+const usersLoadingBtn = document.querySelector('#load-users');
+const usersOnLoad = document.getElementsByClassName('users__link');
 
 async function loadUsers() {
-	const response = await fetch(url);
+	const response = await fetch(`http://localhost:3000/users?_start=0&_end=8`);
 	const data = await response.json();
-	data.forEach((element) => {
-		const {id, name, photo, address:{city}, website} = element
+	await data.forEach((element) => {
+		const {
+			id,
+			name,
+			photo,
+			address: { city },
+			website,
+		} = element;
 		users.insertAdjacentHTML(
 			'beforeend',
 			`<a class="users__link"
@@ -29,5 +37,45 @@ async function loadUsers() {
 		);
 	});
 }
-
 loadUsers();
+
+async function addUsers() {
+	const limit = 8;
+	const response = await fetch(
+		`http://localhost:3000/users?_start=${usersOnLoad.length}&_limit=${limit}`,
+	);
+	const data = await response.json();
+	data.forEach((element) => {
+		const {
+			id,
+			name,
+			photo,
+			address: { city },
+			website,
+		} = element;
+		users.insertAdjacentHTML(
+			'beforeend',
+			`<a class="users__link"
+				href="./pages/user-page.html?id=${id}">
+				<div id=${id} class="users__item">
+					<div class="users__header">
+						<img
+							class="users__img"
+							src="${photo}"
+							alt=""
+						/>
+					</div>
+						<div class="users__main">
+							<h4 class="users__name">${name}</h4>
+							<p class="users__city">City: ${city}</p>
+							<p class="users__website">${website}</p>
+						</div>
+						</div>
+				</a>`,
+		);
+	});
+	if (data.length < limit) {
+		usersLoadingBtn.style.display = 'none';
+	}
+}
+usersLoadingBtn.addEventListener('click', addUsers);
