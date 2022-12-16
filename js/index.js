82a5146a62f0adf2ba517c5d57ffa16b696e617a
holdenn1 <caulfieldd17@gmail.com>
2022-12-16 20:56:26 +0200

    added an age filter

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const users = document.querySelector('.users');
+const filterUsersContainer = document.querySelector('.users-filter-contant');
 const usersLoadingBtn = document.querySelector('#load-users');
-const filterBtn = document.querySelector('.filter-btn');
-const filter = document.querySelector('.filter');
 const usersOnLoad = document.getElementsByClassName('users__link');
+const filter = document.querySelector('.filter');
+const searchBtn = document.querySelector('.search-btn');
+const ageInput = document.querySelector('.filter__age-input');
 
 async function loadUsers() {
 	const response = await fetch(`http://localhost:3000/users?_start=0&_end=8`);
@@ -98,3 +100,62 @@ function showFilter() {
 }
 
 document.addEventListener('click', showFilter);
+
+async function searchUsers() {
+	users.remove();
+	filterUsersContainer.innerHTML = '';
+	const response = await fetch(`http://localhost:3000/users`);
+	const data = await response.json();
+	const inputValue = +ageInput.value;
+	const findUsers = data.filter((data) => data.age == inputValue);
+	if (inputValue == 0) {
+		alert('Set the search age');
+	}
+	if (findUsers.length == 0) {
+		alert('No users found');
+	}
+	findUsers.forEach((element) => {
+		const {
+			id,
+			name,
+			age,
+			photo,
+			address: { city },
+			website,
+		} = element;
+		filterUsersContainer.insertAdjacentHTML(
+			'beforeend',
+			`<a class="users__link"
+				target="_blank"
+				href="./pages/user-page.html?id=${id}">
+				<div id=${id} class="users__item">
+					<div class="users__header">
+						<img
+							class="users__img"
+							src="${photo}"
+							alt=""
+						/>
+					</div>
+						<div class="users__main">
+							<h4 class="users__name">${name}</h4>
+							<p class="users__age">${age} y. o.</p>
+							<p class="users__city">City: ${city}</p>
+							<p class="users__website">${website}</p>
+						</div>
+						</div>
+				</a>`,
+		);
+	});
+	if (findUsers.length < 8) {
+		usersLoadingBtn.style.display = 'none';
+	}
+	clearInput();
+}
+searchBtn.addEventListener('click', () => {
+	filter.classList.remove('filter_active');
+	searchUsers();
+});
+
+function clearInput() {
+	ageInput.value = '';
+}
