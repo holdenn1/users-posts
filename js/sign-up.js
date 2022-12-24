acee6826ac76a2a66792fd2236abd3ff403cc7f3
holdenn1 <caulfieldd17@gmail.com>
2022-12-24 20:13:30 +0200

    added slide form

--- js/sign-up.js
+++ js/sign-up.js
@@ -3,17 +3,55 @@
 const signUpBtn = document.querySelector('.sign-up-btn');
 const avatarImg = document.querySelector('#avatar');
 const avatarFile = document.querySelector('#avatar-file');
+const formItems = document.getElementsByClassName('sign-up-form__item');
+const emailInput = document.querySelector('.sign-up-form-email__email-input');
+const passwordInput = document.querySelector('.sign-up-form-email__password-input');
+console.log(emailInput.value);
+
 const signForm = document.forms.signUp;
-const passwordInput = signForm.password;
-const emailInput = signForm.email;
-const continueBtn = signForm.continue;
 
 window.addEventListener('load', () => {
 	document.addEventListener('click', showsignUpForm);
-	avatarFile.addEventListener('change', loadAvatar)
+	avatarFile.addEventListener('change', loadAvatar);
+	signForm.addEventListener('click', () => {
+		slideItemForm();
+		showCurrentStap();
+	});
+});
 
-})
+let formItemsArr = [...formItems];
 
+let currentStap = formItemsArr.findIndex((i) => {
+	return i.classList.contains('sign-up-form__item_active');
+});
+
+if (currentStap < 0) {
+	currentStap = 0;
+	showCurrentStap();
+}
+
+function slideItemForm() {
+	if (
+		event.target.closest('.sign-up-form__continue-btn') ||
+		event.target.closest('.sign-up-form__buttons-next')
+	) {
+		currentStap += 1;
+	} else if (
+		event.target.closest('.sign-up-form__buttons-prev') ||
+		event.target.closest('.sign-up-form-avatar__buttons-prev')
+	) {
+		currentStap -= 1;
+	}
+	showCurrentStap();
+}
+
+function showCurrentStap() {
+	formItemsArr.forEach((item, index) => {
+		item.classList.toggle('sign-up-form__item_active', index === currentStap);
+	});
+}
+
+/* ========================================================= */
 
 function showsignUpForm() {
 	if (event.target.closest('.sign-up-btn')) {
@@ -27,6 +65,6 @@ function showsignUpForm() {
 function loadAvatar() {
 	if (this.files && this.files[0]) {
 		avatarImg.onload = () => URL.revokeObjectURL(avatarImg.src);
-		avatarImg.src = URL.createObjectURL(this.files[0])
+		avatarImg.src = URL.createObjectURL(this.files[0]);
 	}
 }
