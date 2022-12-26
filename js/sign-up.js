8eb44dec4fc071e0102d5e4a8e4260c434f63a37
holdenn1 <caulfieldd17@gmail.com>
2022-12-26 02:20:37 +0200

    changed form slider

--- js/sign-up.js
+++ js/sign-up.js
@@ -3,62 +3,50 @@
 const signUpBtn = document.querySelector('.sign-up-btn');
 const avatarImg = document.querySelector('#avatar');
 const avatarFile = document.querySelector('#avatar-file');
-const formItems = document.getElementsByClassName('sign-up-form__item');
-const emailInput = document.querySelector('.sign-up-form-email__email-input');
-const passwordInput = document.querySelector('.sign-up-form-email__password-input');
-console.log(emailInput.value);
+const formItems = [...document.getElementsByClassName('sign-up-form__item')];
 
-const signForm = document.forms.signUp;
+const mainForm = document.forms.signUp;
 
 window.addEventListener('load', () => {
 	document.addEventListener('click', showsignUpForm);
 	avatarFile.addEventListener('change', loadAvatar);
-	signForm.addEventListener('click', () => {
-		slideItemForm();
-		showCurrentStap();
+	mainForm.addEventListener('click', () => {
+		sliderNavigation();
+		showCurrentItem()
 	});
 });
 
-let formItemsArr = [...formItems];
+let formItem = 0;
 
-let currentStap = formItemsArr.findIndex((i) => {
-	return i.classList.contains('sign-up-form__item_active');
-});
-
-if (currentStap < 0) {
-	currentStap = 0;
-	showCurrentStap();
-}
-
-function slideItemForm() {
+function sliderNavigation() {
 	if (
 		event.target.closest('.sign-up-form__continue-btn') ||
 		event.target.closest('.sign-up-form__buttons-next')
 	) {
-		currentStap += 1;
+		formItem++;
 	} else if (
 		event.target.closest('.sign-up-form__buttons-prev') ||
 		event.target.closest('.sign-up-form-avatar__buttons-prev')
 	) {
-		currentStap -= 1;
+		formItem--;
 	}
-	showCurrentStap();
 }
 
-function showCurrentStap() {
-	formItemsArr.forEach((item, index) => {
-		item.classList.toggle('sign-up-form__item_active', index === currentStap);
+function showCurrentItem() {
+	formItems.forEach((item) => {
+		item.classList.contains('sign-up-form__item_active') &&
+			item.classList.remove('sign-up-form__item_active');
 	});
+	formItems[formItem].classList.add('sign-up-form__item_active')
 }
 
-/* ========================================================= */
 
 function showsignUpForm() {
 	if (event.target.closest('.sign-up-btn')) {
-		signForm.classList.toggle('sign-up-form_active');
+		mainForm.classList.toggle('sign-up-form_active');
 	}
 	if (!event.target.closest('.sign-up-btn') && !event.target.closest('.sign-up-form')) {
-		signForm.classList.remove('sign-up-form_active');
+		mainForm.classList.remove('sign-up-form_active');
 	}
 }
 
