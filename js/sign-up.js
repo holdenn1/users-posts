767d39bd499812c1bb7498a229492b988813a654
holdenn1 <caulfieldd17@gmail.com>
2022-12-29 18:22:41 +0200

    added find location

--- js/sign-up.js
+++ js/sign-up.js
@@ -10,6 +10,16 @@ const mainForm = document.forms.signUp;
 const emailInput = mainForm.email;
 const passwordInput = mainForm.password;
 const nameInput = mainForm.name;
+const ganderChoose = mainForm.gender;
+const birthDateChoose = mainForm.birthDate;
+const orientationChoose = mainForm.orientation;
+const demandsChoose = [...mainForm.demand];
+const photoFile = mainForm.photoFile;
+const submitBtn = mainForm.submit;
+
+let formItemCount = 0;
+let userInfo = {};
+let demandsAarr = [];
 
 window.addEventListener('load', () => {
 	document.addEventListener('click', showsignUpForm);
@@ -18,10 +28,18 @@ window.addEventListener('load', () => {
 		sliderNavigation();
 		showCurrentItem();
 	});
+	submitBtn.addEventListener('click', () => {
+		if (validateArr[4][0].validate()) {
+			findLocation();
+			console.log(userInfo);
+			mainForm.classList.remove('sign-up-form_active');
+		}
+	});
 });
 
-let formItemCount = 0;
-const validateForm = [
+/*valdate form items*/
+
+const validateArr = [
 	[
 		{
 			validateId: 'emailInput',
@@ -35,6 +53,7 @@ const validateForm = [
 					}, 3000);
 					return false;
 				}
+				userInfo.email = emailInput.value;
 				return true;
 			},
 		},
@@ -44,7 +63,7 @@ const validateForm = [
 				if (!checkPassword(passwordInput)) {
 					passwordInput.classList.add('invalid');
 					errMessage.textContent = passwordInput.value
-						? 'Password must contain at least 6 characters, at least one letter, one number and one special character'
+						? 'Password must contain at least six characters, at least one letter, one number and one special character'
 						: 'Password is requred field';
 					errMessage.style.top = '10px';
 					setTimeout(() => {
@@ -52,18 +71,119 @@ const validateForm = [
 					}, 3000);
 					return false;
 				}
+				userInfo.password = passwordInput.value;
 				return true;
 			},
 		},
 	],
 	[
-		{ validateId: 'nameInput', validate: () => console.log('Hi3!') },
-		{ validateId: 'ganderChoose', validate: () => console.log('Hi4!') },
-		{ validateId: 'birthDate', validate: () => console.log('Hi5!') },
+		{
+			validateId: 'nameInput',
+			validate: () => {
+				if (checkOnEmptyRow(nameInput)) {
+					nameInput.classList.add('invalid');
+					errMessage.textContent = 'Name is requred field';
+					errMessage.style.top = '10px';
+					setTimeout(() => {
+						errMessage.style.top = '-70px';
+					}, 3000);
+					return false;
+				}
+				userInfo.name = nameInput.value;
+				return true;
+			},
+		},
+		{
+			validateId: 'ganderChoose',
+			validate: () => {
+				if (ganderChoose.value == '') {
+					errMessage.textContent = 'Choose your gender please, this is requred field';
+					errMessage.style.top = '10px';
+					setTimeout(() => {
+						errMessage.style.top = '-70px';
+					}, 3000);
+					return false;
+				}
+				userInfo.gander = ganderChoose.value;
+				return true;
+			},
+		},
+		{
+			validateId: 'birthDate',
+			validate: () => {
+				if (birthDateChoose.value == '') {
+					errMessage.textContent = 'Please indicate your birthday';
+					errMessage.style.top = '10px';
+					setTimeout(() => {
+						errMessage.style.top = '-70px';
+					}, 3000);
+					return false;
+				}
+				if (birthDateChoose.value > '2016-01-01') {
+					errMessage.textContent = 'You cannot be younger than six years old';
+					errMessage.style.top = '10px';
+					setTimeout(() => {
+						errMessage.style.top = '-70px';
+					}, 3000);
+					return false;
+				}
+				userInfo.birth = birthDateChoose.value;
+				return true;
+			},
+		},
 	],
-	[{ validateId: 'orientationChoose', validate: () => console.log('Hi6!') }],
-	[{ validateId: 'demandsChoode', validate: () => console.log('Hi6!') }],
-	[{ validateId: 'uploadPhoto', validate: () => console.log('Hi6!') }],
+	[
+		{
+			validateId: 'orientationChoose',
+			validate: () => {
+				if (orientationChoose.value == '') {
+					errMessage.textContent = 'This field is required';
+					errMessage.style.top = '10px';
+					setTimeout(() => {
+						errMessage.style.top = '-70px';
+					}, 3000);
+					return false;
+				}
+				userInfo.orientation = orientationChoose.value;
+				return true;
+			},
+		},
+	],
+	[
+		{
+			validateId: 'demandsChoode',
+			validate: () => {
+				if (findDemand().length < 1) {
+					errMessage.textContent = 'You must choose at least one demand';
+					errMessage.style.top = '10px';
+					setTimeout(() => {
+						errMessage.style.top = '-70px';
+					}, 3000);
+					return false;
+				}
+				findDemand().forEach((i) => demandsAarr.push(i.id));
+				userInfo.demands = demandsAarr;
+				return true;
+			},
+		},
+	],
+	[
+		{
+			validateId: 'uploadPhoto',
+			validate: () => {
+				if (photoFile.value == '') {
+					errMessage.textContent = 'Upload photo';
+					errMessage.style.top = '10px';
+					setTimeout(() => {
+						errMessage.style.top = '-70px';
+					}, 3000);
+					return false;
+				}
+				userInfo.photo = photoFile.value;
+				return true;
+			},
+		},
+	],
 ];
 
 function sliderNavigation() {
@@ -71,8 +191,9 @@ function sliderNavigation() {
 		event.target.closest('.sign-up-form__continue-btn') ||
 		event.target.closest('.sign-up-form__buttons-next')
 	) {
-		if (test()) {
+		if (validateForm(formItemCount)) {
 			formItemCount++;
+			console.log(formItemCount);
 		}
 	} else if (
 		event.target.closest('.sign-up-form__buttons-prev') ||
@@ -82,14 +203,29 @@ function sliderNavigation() {
 	}
 }
 
-const validateArr = validateForm[formItemCount];
-
-function test() {
-	let checkValue = validateArr.every((i) => i.validate());
+function validateForm(index) {
+	let validateArrItem = validateArr[index];
+	let checkValue = validateArrItem.every((i) => i.validate());
 	return checkValue;
 }
 
-/* regular expression on cheks */
+function findLocation() {
+	navigator.geolocation.getCurrentPosition((position) => {
+		let lat = position.coords.latitude;
+		let long = position.coords.longitude;
+		fetch(
+			`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${long}&format=json&addressdetails=1&accept-language=en`,
+		)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				userInfo.address = data;
+			});
+	});
+}
+
+/* check function on cheks */
 function checkOnEmptyRow(name) {
 	return /^\s*$/.test(name.value);
 }
@@ -104,6 +240,10 @@ function checkPassword(pass) {
 	return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/.test(pass.value);
 }
 
+function findDemand() {
+	return demandsChoose.filter((i) => i.checked == true);
+}
+
 /* Check email and passworn on focus */
 emailInput.addEventListener('focus', () => {
 	if (emailInput.classList.contains('invalid')) {
@@ -114,6 +254,12 @@ emailInput.addEventListener('focus', () => {
 passwordInput.addEventListener('focus', () => {
 	if (passwordInput.classList.contains('invalid')) {
 		passwordInput.classList.remove('invalid');
+	}
+});
+
+nameInput.addEventListener('focus', () => {
+	if (nameInput.classList.contains('invalid')) {
+		nameInput.classList.remove('invalid');
 	}
 });
 
@@ -133,6 +279,15 @@ passwordInput.addEventListener('input', () => {
 	}
 	if (checkPassword(passwordInput)) {
 		passwordInput.classList.remove('invalid');
+	}
+});
+
+nameInput.addEventListener('input', () => {
+	if (checkOnEmptyRow(nameInput)) {
+		nameInput.classList.add('invalid');
+	}
+	if (!checkOnEmptyRow(nameInput)) {
+		nameInput.classList.remove('invalid');
 	}
 });
 //=========================================================================================
